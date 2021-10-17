f49f7240d0253d4d8d48511d9022ce349781bf3d
sirriah <tereza.dvorakova.3@seznam.cz>
2021-10-17 13:35:59 +0200

    masonry grid

---
+++ js/masonry.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-undef
+const macyInstance = Macy({
+  // See below for all available options.
+  container: '.grid',
+  columns: 4,
+  margin: 40,
+  breakAt: {
+    1100: 3,
+    840: 2,
+    600: 1,
+  },
+});
