2cf007a3e23243265ad3b27fe4ad1b53626cd216
sirriah <tereza.dvorakova.3@seznam.cz>
2021-10-20 14:00:28 +0200

    detail grid

--- js/home.js
+++ js/home.js
@@ -1,22 +1,3 @@
-/*
-// this is the path to the json file
-let requestURL = '../data.json';
-// this is a object instance of XMLHttpRequest
-let request = new XMLHttpRequest();
-
-// HTTP menthod to use when making the network request / GET
-request.open('GET', requestURL);
-// JSON is converted into the JS object
-request.responseType = 'json';
-// send a request with the send method
-request.send();
-
-request.onload = () => {
-  const paintingsData = request.response;
-  console.log(paintingsData);
-};
-*/
-
 fetch('../data.json')
   .then((response) => response.json())
   .then((data) => console.log(data));
