f9797dee288014b471e64b52c779727ca1726faa
sirriah <tereza.dvorakova.3@seznam.cz>
2021-11-05 14:26:10 +0100

    masonry fixed

--- js/home.js
+++ js/home.js
@@ -8,16 +8,26 @@ const displayData = (data) => {
   data.forEach((element, index) => {
     grid.innerHTML += `
     <section class="image-wrapper grid-item">
-    <a class="grid-item__link" href="detail.html?painting=${index}" data-id="${element.key}">
-    <div class="grid-item__text">
-    <h2 class="heading heading--2 white">${element.name}</h2>
-    <p class="subhead subhead--2 white--opacity">${element.artist.name}</p>
-  </div>
-  <img class="grid-item__image" src="${element.images.thumbnail}" alt="${element.name}, author ${element.artist.name}">
-    </a>
-  </section>`;
+      <a class="grid-item__link" href="detail.html?painting=${index}" data-id="${element.key}">
+        <div class="grid-item__text">
+          <h2 class="heading heading--2 white">${element.name}</h2>
+          <p class="subhead subhead--2 white--opacity">${element.artist.name}</p>
+        </div>
+        <img class="grid-item__image" src="${element.images.thumbnail}" alt="${element.name}, author ${element.artist.name}">
+      </a>
+    </section>`;
 
     paintingsStorage.setItem(index, JSON.stringify(element));
+  });
+
+  // masonry snippet
+  const $grid = $('.grid').imagesLoaded(() => {
+    // init Masonry after all images have loaded
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      columnWidth: 160,
+      gutter: 10,
+    });
   });
 };
 
